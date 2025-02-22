aff6f1ca562ca5ea0c200dea0581ca7711fc6d83
Fardin-Mohammed <39059@ma-web.nl>
2025-02-22 16:08:40 +0100

    6 is af

---
+++ m3prog_project/public/01/variabelennaam.php
@@ -0,0 +1,26 @@
+<?php
+$auto2 = "rs7";
+echo "<br/>";
+echo $auto2;
+$_naam = "Fardin";
+echo "<br/>";
+echo $_naam;
+//$auto's = "ferrari";
+//echo $auto's;
+// $auto/onderdelen = "uitlaat";
+// echo $auto/onderdelen;
+// $huis&tuin = "grass";
+// echo $huis&tuin;
+$naw = "wan";
+echo "<br/>";
+echo $naw;
+// $22 = "21";
+// echo $22;
+// $$auto = "Lamborghini";
+// echo $$auto;
+$kleurVanAuto = "Black";
+echo "<br/>";
+echo $kleurVanAuto;
+// $software-dev = "ik";
+// echo $software-dev;
+?>
