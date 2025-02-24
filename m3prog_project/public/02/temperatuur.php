007baa706149846254b0018fa65a712a03fe1c43
Fardin-Mohammed <39059@ma-web.nl>
2025-02-24 12:52:04 +0100

    05 rekenen af

---
+++ m3prog_project/public/02/temperatuur.php
@@ -0,0 +1,21 @@
+<?php
+// Fahrenheit naar Celsius
+$fahrenheit = 74;
+$celsius = ($fahrenheit - 32) / 1.8;
+echo "{$fahrenheit} graden Fahrenheit = {$celsius} graden Celsius.  <br/>";
+
+// Celsius naar Fahrenheit
+$celsius = 34;
+$fahrenheit = 93.2;
+echo "{$celsius} graden Celsius = {$fahrenheit} graden Fahrenheit.  <br/>";
+
+// Celsius naar Kelvin
+$celsius = 27;
+$kelvin = 300.15;
+echo "{$celsius} graden Celsius = {$kelvin} graden Kelvin.  <br/>";
+
+// Fahrenheit naar Kelvin
+$fahrenheit = 100;
+$kelvin = 310.93;
+echo "{$fahrenheit} graden Fahrenheit = {$kelvin} graden Kelvin.  <br/>";
+?>
