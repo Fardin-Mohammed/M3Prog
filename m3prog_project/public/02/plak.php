3d512ca5c03fb12eb37c44c03aadc860af4fe4cf
Fardin-Mohammed <39059@ma-web.nl>
2025-02-24 14:10:45 +0100

    02 insert af

--- m3prog_project/public/02/plak.php
+++ m3prog_project/public/02/plak.php
@@ -6,9 +6,32 @@ $extraString = "extra world<br>";
 $voornaamString = 'luigi';
 $achternaamString = 'mario';
 $fullname = "Luigi" ." ". "mario  <br>";
+$naam = 'Yoshi';
+$ondertekentDoor = 'directeur bowser';
+$enemy = een andere vijand dan Goomba;
 // De br zit erin om afstand te maken tussen de variabels
 
 echo $standaardString;
 echo $extraString;
 echo $fullname;
+
+echo "<br/>";
+
+
+
+
+
+
+$emailTekst = "
+Beste $naam<br>
+Helaas moeten wij uw verzoek afwijzen.<br>
+Onze Koopa Troopa`s vinden het niet leuk om opgegeten te worden<br>
+<br>
+Hoogachtend<br>
+$ondertekentDoor<br>
+<br>
+PS:<br>
+De princess is in haar kasteel... voor nu :>
+";
+echo $emailTekst;
 ?>
