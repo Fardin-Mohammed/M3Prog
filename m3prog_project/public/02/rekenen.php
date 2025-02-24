966ef8e705edac5292546308309cc9f30a817b37
Fardin-Mohammed <39059@ma-web.nl>
2025-02-24 12:34:03 +0100

    04 rekenen af

---
+++ m3prog_project/public/02/rekenen.php
@@ -0,0 +1,34 @@
+<?php
+$getal = 1.12345;
+$afgerond1 = round($getal);
+$getal2 = 1.12345;
+$afgerond2 = floor($getal2);
+$qwerty1 = rand();
+$qwerty2 = rand();
+$qwerty3 = rand();
+$random1 = 2130219144;
+$random2 = 1654185290;
+$random3 = 775197102;
+$uitkomst1 = ($random1+$random2);
+$uitkomst2 = (3784404434/$random3);
+$uitkomst3 = 4.8818609154192;
+
+echo $getal;
+echo "<br/>";
+echo $afgerond1;
+echo $getal2;
+echo "<br/>";
+echo $afgerond2;
+echo "<br/>";
+echo $qwerty1;
+echo "<br/>";
+echo $qwerty2;
+echo "<br/>";
+echo $qwerty3;
+echo "<br/>";
+echo $uitkomst1;
+echo "<br/>";
+echo $uitkomst2;
+echo "<br/>";
+echo $uitkomst3;
+?>
