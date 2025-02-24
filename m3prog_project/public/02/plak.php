9a71065013c4c68ce1f5a3d7fda7fc44799eb72f
Fardin-Mohammed <39059@ma-web.nl>
2025-02-24 13:05:06 +0100

    01 strings van 02 af

---
+++ m3prog_project/public/02/plak.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$standaardString = 'hello world<br>';
+$extraString = "extra world<br>";
+$voornaamString = 'luigi';
+$achternaamString = 'mario';
+$fullname = "Luigi" ." ". "mario  <br>";
+// De br zit erin om afstand te maken tussen de variabels
+
+echo $standaardString;
+echo $extraString;
+echo $fullname;
+?>
