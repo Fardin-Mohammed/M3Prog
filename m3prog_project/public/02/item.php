a13c91e36d4ce4793c6863afa52bc0b776e59caf
Fardin-Mohammed <39059@ma-web.nl>
2025-03-02 22:22:15 +0100

    06 item displa van 02 is af en nu beide 02 af

---
+++ m3prog_project/public/02/item.php
@@ -0,0 +1,41 @@
+<?php
+$hoofd = 'Immortal Kings Pillar (War Boots)';
+$defense = 'Defense: 118-288';
+$level = 'Required Level: 31';
+$strength = 'Required Strength: 125';
+$durability = 'Durability: 24';
+
+$hoofd2 = 'Stat Bonuses:';
+$bonus1 = '+40% Faster Run/Walk';
+$bonus2 = '+75 Defense';
+$bonus3 = '+110 To Attack Rating';
+$bonus4 = '+44 To Life';
+
+
+echo "<h1>$hoofd</h1>";
+echo "</br>";
+echo $defense;
+echo "</br>";
+echo "</br>";
+echo $level;
+echo "</br>";
+echo "</br>";
+echo $strength;
+echo "</br>";
+echo "</br>";
+echo $durability;
+echo "</br>";
+echo "</br>";
+echo "</br>";
+echo "<h2>$hoofd2</h2>";
+echo $bonus1;
+echo "</br>";
+echo "</br>";
+echo $bonus2;
+echo "</br>";
+echo "</br>";
+echo $bonus3;
+echo "</br>";
+echo "</br>";
+echo $bonus4;
+?>
