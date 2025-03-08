b5664cb2f87c5b4abbffbec95ae6506c122ef6ef
Fardin-Mohammed <39059@ma-web.nl>
2025-03-08 16:34:47 +0100

    02 van 03 is af

--- m3prog_project/public/03/if.php
+++ m3prog_project/public/03/if.php
@@ -1,6 +1,6 @@
 <?php
 
-// Variabele $opvoorraad met een boolean waarde
+// Stap 1: Variabele $opvoorraad met een boolean waarde
 $opvoorraad = false;
 
 // Controleren of het product niet op voorraad is
@@ -10,14 +10,17 @@ if ($opvoorraad == false) {
 
 echo "<br>";
 
-// Nieuwe variabele voor aantal op voorraad
+// Stap 2: Nieuwe variabele voor aantal op voorraad
 $aantalOpVoorraad = 20;
 
 // Opnieuw bepalen of het product op voorraad is
 $opvoorraad = ($aantalOpVoorraad > 0);
 
-// Controleren of het product niet op voorraad is
-if ($opvoorraad == false) {
+// If-else statement om voorraadstatus te tonen
+if ($aantalOpVoorraad > 0) {
+    echo "Artikel is op voorraad";
+} else {
     echo "Niet op voorraad";
 }
+
 ?>
