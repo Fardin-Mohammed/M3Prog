706ed00a2ab8da83bf9b4f5e364b54520bfb3b64
Fardin-Mohammed <39059@ma-web.nl>
2025-03-08 16:19:43 +0100

    01 van 03 is af

---
+++ m3prog_project/public/03/elseif.php
@@ -0,0 +1,33 @@
+<?php
+
+//R = rood, O= oranje, G = groen
+$stopLichtKleur = 'R';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // nieuwe php tag
+        if()//als groen
+        {
+
+        }
+        else if()// als oranje
+        {
+
+        }
+        else//als rood
+        {
+
+        }
+        <button style='background-color:#FF0000;height:25px;width:25px;'/>
+    ?>
+</body>
+</html>
--- m3prog_project/public/03/if.php
+++ m3prog_project/public/03/if.php
@@ -1,16 +1,23 @@
 <?php
-$opvoorraad = 0;    
-$aantalOpVoorraad = 0;
-$opvoorraad = $aantalOpVoorraad > 0;
 
+// Variabele $opvoorraad met een boolean waarde
+$opvoorraad = false;
 
-if(false)
-{
-    echo  "niet op voorraad";
+// Controleren of het product niet op voorraad is
+if ($opvoorraad == false) {
+    echo "Niet op voorraad";
 }
 
-if($opvoorraad == true)
-{
+echo "<br>";
 
+// Nieuwe variabele voor aantal op voorraad
+$aantalOpVoorraad = 20;
+
+// Opnieuw bepalen of het product op voorraad is
+$opvoorraad = ($aantalOpVoorraad > 0);
+
+// Controleren of het product niet op voorraad is
+if ($opvoorraad == false) {
+    echo "Niet op voorraad";
 }
 ?>
