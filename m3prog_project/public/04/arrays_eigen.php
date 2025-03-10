12db63df6595b7d0e9bf8408c5e1fe9cd8aded49
Fardin-Mohammed <39059@ma-web.nl>
2025-03-10 12:26:02 +0100

    03 van 04 is af

---
+++ m3prog_project/public/04/arrays_eigen.php
@@ -0,0 +1,16 @@
+<?php
+$games = ["Gta","Fortnite","Minecraft","COD","Fifa"];
+print_r($games);
+echo"<br>";
+echo $games[2];
+echo "<br>";
+echo $games[4];
+echo "<br>";
+echo$namen;
+echo "<br>";
+print_r($games);
+echo "<br>";
+echo "<br>";
+echo "<br>";
+var_dump($games);
+?>
