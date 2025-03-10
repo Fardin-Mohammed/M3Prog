eac23f45a5b9c08694d496ec0dd68283e0eb047b
Fardin-Mohammed <39059@ma-web.nl>
2025-03-10 12:15:05 +0100

    02 van 04 af

--- m3prog_project/public/04/arrays_uitleg.php
+++ m3prog_project/public/04/arrays_uitleg.php
@@ -1,8 +1,24 @@
 <?php
-$nummers = [1,2,3];
+$nummers = [1,2,3,88,99,55,66,11,345,578,34525];
 print_r($nummers);
 
 
-$extraNummers = [1,2,445,667,875,43,6,876543,];
-print_r($extraNummers);
+
+$dagen = ["maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag"];
+echo"<br>";
+echo $dagen[4];
+echo "<br>";
+echo $dagen[2];
+echo "<br>";
+echo $dagen[6];
+echo "<br>";
+echo $dagen[1];
+echo "<br>";
+echo $nummers[9];
+echo "<br>";
+echo $nummers[1];
+echo "<br>";
+echo $nummers[0];
+echo "<br>";
+echo $nummers[4];
 ?>
