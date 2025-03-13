1daebac280cd75576f2bbe7970a0545be2d3e39c
Fardin-Mohammed <39059@ma-web.nl>
2025-03-13 13:41:10 +0100

    01 van 05 is af

---
+++ m3prog_project/public/05/phpfunctions.php
@@ -0,0 +1,27 @@
+<?php
+$hoeLangIsDitWoord = "scrabbleWoord";
+$woordLengte = strlen($hoeLangIsDitWoord);
+
+print($woordLengte);
+
+
+
+//Vraag 1
+// een string
+
+//Vraag 2
+//een integer(int)
+
+//Vraag 3
+//de lengte van de string
+
+echo "<br>";
+
+$anderLangWoord = "skibidi";
+$reactie = strlen($anderLangWoord);
+
+print($reactie);
+
+//Vraag 4
+//2 keer denk ik
+?>
