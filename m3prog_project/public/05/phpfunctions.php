2d5f4eb75f489e73b0d5903b62fccd2f6ec3e3df
Fardin-Mohammed <39059@ma-web.nl>
2025-03-13 13:49:52 +0100

    01/02 van 05 is af

--- m3prog_project/public/05/phpfunctions.php
+++ m3prog_project/public/05/phpfunctions.php
@@ -1,19 +1,9 @@
 <?php
+// Berekening van de lengte van een woord
 $hoeLangIsDitWoord = "scrabbleWoord";
 $woordLengte = strlen($hoeLangIsDitWoord);
 
 print($woordLengte);
-
-
-
-//Vraag 1
-// een string
-
-//Vraag 2
-//een integer(int)
-
-//Vraag 3
-//de lengte van de string
 
 echo "<br>";
 
@@ -22,6 +12,34 @@ $reactie = strlen($anderLangWoord);
 
 print($reactie);
 
-//Vraag 4
-//2 keer denk ik
+echo "<br>";
+
+$tweeMacht2 = pow(2, 10);
+print $tweeMacht2;
+
+echo "<br>";
+
+$drieMacht4 = pow(3, 4);
+print $drieMacht4;
+
+echo "<br>";
+
+print pow(3, 4);
+
+echo "<br>";
+
+$naam = "Wario";
+$naamGroot = strtoupper($naam);
+print $naamGroot;
+
+echo "<br>";
+
+
+$naamKlein = strtolower($naamGroot);
+print $naamKlein;
+
+echo "<br>";
+
+print $naam;
+
 ?>
