7c81be18d694d49a5e1c8a700dd999ec831154fc
Fardin-Mohammed <39059@ma-web.nl>
2025-04-15 13:56:28 +0200

    01 van 07 is af

---
+++ m3prog_project/public/07/assoc_intro.php
@@ -0,0 +1,26 @@
+<?php
+$trophiesPerGame = [
+    "Demon`s souls" => 11,
+    "Mario wonder" => 0,
+    "Rayman Origins" => 9,
+    "Tetris" => 10
+];
+
+$trophiesForDS = $trophiesPerGame["Demon`s souls"];
+echo $trophiesForDS;
+
+echo"<br>";
+
+$tp = $trophiesPerGame["Mario wonder"];
+echo $tp;
+
+echo"<br>";
+
+$tp2 = $trophiesPerGame["Rayman Origins"];
+echo $tp2;
+
+echo"<br>";
+
+$tp3 = $trophiesPerGame["Tetris"];
+echo $tp3;
+?>
