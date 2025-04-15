101b254cf33acc4eceb9824f8df3865a5a6cb3cf
Fardin-Mohammed <39059@ma-web.nl>
2025-04-15 14:06:15 +0200

    02 van 07 is af

---
+++ m3prog_project/public/07/personen.php
@@ -0,0 +1,33 @@
+<?php
+$persoon = [
+    "voornaam" => "Luigi",
+    "achternaam" => "Mario",
+    "leeftijd" => 19,
+    "favoriet eten" => "pasta",
+    "minst favoriet eten" => "paddenstoelen",
+    "woonplaats" => "mushroom kingdom"
+];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Persoon info</title>
+</head>
+<body>
+
+
+<table>
+    <tr>
+        <th>Key</th>
+        <th>Value</th>
+    </tr>
+    <?php
+    foreach ($persoon as $key => $value) {
+        echo "<tr><td>$key</td><td>$value</td></tr>";
+    }
+    ?>
+</table>
+
+</body>
+</html>
